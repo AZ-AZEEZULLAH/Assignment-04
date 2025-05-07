2537a555f4282d564e9062e93d8adf81d600e859
Azeezullah <166276780+AZ-AZEEZULLAH@users.noreply.github.com>
2025-05-07 22:42:03 +0500

    Add new folder and files

---
+++ Assignments 00 to 07/00_intro_python/01_add_two_numbers/add.py
@@ -0,0 +1,29 @@
+## add two numbers
+
+
+print("Add two numbers")
+
+
+
+## ------------------------------------
+
+
+def add():
+    """Add two numbers"""
+    print("This program adds two numbers")
+
+    number1 = int(input("Enter first number: "))
+    number2 = int(input("Enter second number: "))
+
+    total = number1 + number2
+
+   # print(f"The total sum of {number1} and {number2} is {total}")
+    #print("The total number is ",sum)
+    print("The total is " + str(total) + ".")
+
+if __name__ == "__main__":
+    add()
+ 
+
+
+   ## ----------------------------------------------------
